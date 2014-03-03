de2578b02f95b0ca7ede123ca33d9b9fe87a755c
Royce Pope <roycepope@royce-mbp.oakgatetech.com>
2014-03-03 14:40:25 -0800

    v2.0
    Menu added!

--- ex21/ex21.py
+++ ex21/ex21.py
@@ -1,15 +1,26 @@
 # Royce Pope
 # March 3 2014
-# Simple Calculator v1.0
-
-import os
+# Simple Calculator v1.1
 
 def add(a,b):
     print ("=====ADDITION=====")
-    ans = add(num1, num2)
     print ("ADDING %d + %d" % (a, b))
-    print ("Answer = %d" % ans)
     return a + b
+    
+def subtract(a,b):
+    print ("=====SUBTRACT=====")
+    print ("SUBRACTING %d - %d" % (a, b))
+    return a - b
+
+def multiply(a,b):
+    print ("=====MULTIPLY=====")
+    print ("MULTIPLYING %d * %d" % (a, b))
+    return a * b
+    
+def divide(a,b):
+    print ("=====DIVIDE=====")
+    print ("DIVIDING %d / %d" % (a, b))
+    return a / b
     
 print ("""
 ******************************
@@ -20,12 +31,12 @@ print ("""
 *                            *
 ****************************** \n""")
 
+print ("Let's do some Math!")
+
 print("""
 What would you like to do?
-1) Addition
-2) Subtraction
-3) Multiplication
-4) Division
+1) Addition     3) Multiplication
+2) Subtraction  4) Division
 """)
 
 choice = int(input(">> "))
@@ -36,45 +47,17 @@ print ("Please enter 2nd number")
 num2 = int(input(">> "))
 
 if choice == 1:
-    os.system('clear')
-    output = add(num1, num2)
+    answer = add(num1, num2)
+    print ("Answer = %s" % answer)
 
 if choice == 2:
-    subtract()
-def subtract(a,b):
-    print ("SUBRACTING %d - %d" % (a, b))
-    return a - b
+    answer = subtract(num1, num2)
+    print ("Answer = %s" % answer)
+     
+if choice == 3:
+    answer = multiply(num1, num2)
+    print ("Answer = %s" % answer)
 
-def multiply(a,b):
-    print ("MULTIPLYING %d * %d" % (a, b))
-    return a * b
-    
-def divide(a,b):
-    print ("DIVIDING %d / %d" % (a, b))
-    return a / b
-
-print ("Let's do some Math!")
-
-print ("=====SUBTRACT=====")
-print ("Please enter 1st number")
-sub1 = int(input(">> "))
-print ("Please enter 2nd number")
-sub2 = int(input(">> "))
-sub_answer = subtract(sub1, sub2)
-print ("Answer = %d" % sub_answer)
-
-print ("=====MULTIPLY=====")
-print ("Please enter 1st number")
-mul1 = int(input(">> "))
-print ("Please enter 2nd number")
-mul2 = int(input(">> "))
-mul_answer = multiply(mul1, mul2)
-print ("Answer = %d" % mul_answer)
-
-print ("=====DIVIDE=====")
-print ("Please enter 1st number")
-div1 = int(input(">> "))
-print ("Please enter 2nd number")
-div2 = int(input(">> "))
-div_answer = divide(div1, div2)
-print ("Answer = %d" % div_answer)
+if choice == 4:
+    answer = divide(num1, num2)
+    print ("Answer = %s" % answer)
