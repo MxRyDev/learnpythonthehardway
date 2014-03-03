90d39504eb5e3fafc840930a8d091e467a504b25
Royce Pope <roycepope@royce-mbp.oakgatetech.com>
2014-03-03 12:43:56 -0800

    v1.0

--- ex21/ex21.py
+++ ex21/ex21.py
@@ -1,12 +1,13 @@
 # Royce Pope
 # March 3 2014
+# Simple Calculator
 
 def add(a,b):
     print ("ADDING %d + %d" % (a, b))
     return a + b
 
 def subtract(a,b):
-    print ("SUBRACTING %s - %s" % (a, b))
+    print ("SUBRACTING %d - %d" % (a, b))
     return a - b
 
 def multiply(a,b):
@@ -19,35 +20,32 @@ def divide(a,b):
 
 print ("=====ADDITION=====")
 print ("Please enter 1st number")    
-add1 = input()
+add1 = int(input(">> "))
 print ("Please enter 2nd number")
-add2 = input()
+add2 = int(input(">> "))
 add_answer = add(add1, add2)
-print ("Addition Answer=%d" % add_answer)
+print ("Answer = %d" % add_answer)
 
-# This can be used a a Multi-Line comment
-'''
 print ("=====SUBTRACT=====")
 print ("Please enter 1st number")
-sub1 = input()
+sub1 = int(input(">> "))
 print ("Please enter 2nd number")
-sub2 = input()
-
-subtract(sub1, sub2)
+sub2 = int(input(">> "))
+sub_answer = subtract(sub1, sub2)
+print ("Answer = %d" % sub_answer)
 
 print ("=====MULTIPLY=====")
 print ("Please enter 1st number")
-mul1 = input()
+mul1 = int(input(">> "))
 print ("Please enter 2nd number")
-mul2 = input()
-
-multiply(mul1, mul2)
+mul2 = int(input(">> "))
+mul_answer = multiply(mul1, mul2)
+print ("Answer = %d" % mul_answer)
 
 print ("=====DIVIDE=====")
 print ("Please enter 1st number")
-div1 = input()
+div1 = int(input(">> "))
 print ("Please enter 2nd number")
-div2 = input()
-
-divide(div1, div2)
-'''
+div2 = int(input(">> "))
+div_answer = divide(div1, div2)
+print ("Answer = %d" % div_answer)
